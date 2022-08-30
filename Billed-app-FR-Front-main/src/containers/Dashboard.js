fa3038da5f339266c6c02ff48aa0f39cd9917e64
STEPHANE POLETTI <polettistephane@msn.com>
2022-08-30 17:55:41 +0200

    Je suis connecté en tant qu'administrateur RH, je déplie une liste de tickets (par exemple : statut validé), je sélectionne un ticket, puis je déplie une seconde liste (par exemple : statut refusé), je ne peux plus sélectionner un ticket de la première liste.

--- Billed-app-FR-Front-main/src/containers/Dashboard.js
+++ Billed-app-FR-Front-main/src/containers/Dashboard.js
@@ -72,6 +72,11 @@ export default class {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
+    this.dropdownListIsOpen = {
+      1 : false,
+      2 : false,
+      3 : false
+    }
     $('#arrow-icon1').click((e) => this.handleShowTickets(e, bills, 1))
     $('#arrow-icon2').click((e) => this.handleShowTickets(e, bills, 2))
     $('#arrow-icon3').click((e) => this.handleShowTickets(e, bills, 3))
@@ -86,35 +91,30 @@ export default class {
   }
 
     handleEditTicket(e, bill, bills) {
+      // e.preventDefault()
 
-    if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-
-    if (this.id === undefined || this.id !== bill.id) this.id = bill.id
+      if (this.counter === undefined || this.id !== bill.id) this.counter = 0
+      
+      if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     
-    if (this.counter % 2 === 0 ) {
+      if (this.counter % 2 === 0) {
+      
       bills.forEach(b => {
         $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
       })
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      
-      // console.log('counter après le if', this.counter);
-      // console.log('counter après le if', this.counter);
       this.counter ++
       
-    } else if (this.counter % 2 === 0 ) {
+    } else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
       
       $('.dashboard-right-container div').html(`
       <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `)
       $('.vertical-navbar').css({ height: '120vh' })
-      
       this.counter ++
-    }
-    else{
-      this.counter=0
     }
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
@@ -142,22 +142,31 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
+    // e.preventDefault()
+
     if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
+    // if (this.counter % 2 === 0) {
+
+      if (!this.dropdownListIsOpen[index]) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
+        
+        this.dropdownListIsOpen[index] = true
     } else {
+      
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
       $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
+      .html("")
+      
+      this.dropdownListIsOpen[index] = false
     }
 
+
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      // $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#status-bills-container${this.index}  #open-bill${bill.id} `).click((e) => this.handleEditTicket(e, bill, bills))
+
     })
 
     return bills
