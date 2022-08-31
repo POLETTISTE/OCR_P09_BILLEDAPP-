822a59804701ff43fb81a7d818df2e3da4c8fa59
STEPHANE POLETTI <polettistephane@msn.com>
2022-08-31 14:43:56 +0200

    rename test files

--- Billed-app-FR-Front-main/src/__tests__/NewBill.test.js
+++ Billed-app-FR-Front-main/src/__tests__/NewBill.test.js
@@ -9,10 +9,11 @@ import NewBill from "../containers/NewBill.js"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+    test("Then I can see the submit button", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
+      expect(screen.getAllByText('Envoyer une note de frais')).toBeTruthy()
     })
   })
 })
